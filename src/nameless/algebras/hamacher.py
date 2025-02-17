e0671ccf9e086eed073fe556e8ecebb13239c3a8
konstantinosKokos <konstantinos@riseup.net>
2025-02-17 19:39:59 +0100

    no idea which Hamacher to use

--- src/nameless/algebras/hamacher.py
+++ src/nameless/algebras/hamacher.py
@@ -13,15 +13,15 @@ class Hamacher(Algebra):
         self.register_buffer('_bottom', torch.tensor(0.))
         self.properties = Properties.check(self)
 
+    # todo: check these.
     def meet(self, x: Tensor, y: Tensor) -> Tensor:
-        return torch.where((x == 0) | (y == 0) , 0., (x + y) / (x + y - x * y))
+        return torch.where((x == 0) | (y == 0), 0., (x * y) / (x + y - x * y))
 
     def join(self, x: Tensor, y: Tensor) -> Tensor:
-        return (x + y) / ( 1 + x + y)
+        return (x + y - x * y) / (1 - x * y)
 
     def implies(self, x: Tensor, y: Tensor) -> Tensor:
-        return torch.where(x == 0, self.top, (1 - x + y) / (1 + y))
+        return torch.where(x == 0, self.top, (1 - x + y) / (1 - x * y))
 
     def neg(self, x: Tensor) -> Tensor:
         return 1 - x
-
