f8dde751141a7330988f1694f53e86e400101c3d
konstantinosKokos <konstantinos@riseup.net>
2025-02-17 14:35:59 +0100

    add dtype specification, infer properties on demand

--- src/nameless/algebras.py
+++ src/nameless/algebras.py
@@ -5,6 +5,8 @@ from typing import Callable as Fn, Type
 from abc import ABC, abstractmethod
 from itertools import accumulate
 from functools import reduce
+from dataclasses import dataclass
+
 
 def scan(fn: Fn[[Tensor, Tensor], Tensor], default: Tensor) -> Fn[[Tensor], Tensor]:
     def f(x: Tensor) -> Tensor:
@@ -18,7 +20,29 @@ def fold(fn: Fn[[Tensor, Tensor], Tensor], initial: Tensor) -> Fn[[Tensor], Tens
     return f
 
 
-class Algebra(ABC):
+@dataclass(eq=True)
+class Properties:
+    meet_commutative: bool
+    join_commutative: bool
+    meet_associative: bool
+    join_associative: bool
+    meet_idempotent: bool
+    join_idempotent: bool
+    absorption: bool
+    distributivity: bool
+    involution: bool
+    de_morgan: bool
+    complementarity: bool
+
+    def __repr__(self) -> str:
+        lines = [f"\t{name:<25}: {value}" for name, value in self.__dict__.items()]
+        return "\n".join(lines)
+
+
+class Algebra[T](ABC):
+    dtype = T
+    properties: Properties = ...
+
     @classmethod
     @abstractmethod
     def top(cls) -> Tensor: ...
@@ -46,9 +70,73 @@ class Algebra(ABC):
     @classmethod
     @abstractmethod
     def forall(cls, x: Tensor) -> Tensor: ...
-    
-    
+
+
+def commutative(fn: Fn[[Tensor, Tensor], Tensor]) -> Fn[[Tensor, Tensor], bool]:
+    def f(x: Tensor, y: Tensor) -> bool:
+        return torch.allclose(fn(x, y), fn(y, x))
+    return f
+
+def associative(fn: Fn[[Tensor, Tensor], Tensor]) -> Fn[[Tensor, Tensor, Tensor], bool]:
+    def f(x: Tensor, y: Tensor, z: Tensor) -> bool:
+        return torch.allclose(fn(x, fn(y, z)), fn(fn(x, y), z))
+    return f
+
+def idempotent(fn: Fn[[Tensor, Tensor], Tensor]) -> Fn[[Tensor], bool]:
+    def f(x: Tensor) -> bool:
+        return torch.allclose(fn(x, x), x)
+    return f
+
+
+def check(algebra: Type[Algebra], test_size: int = 50) -> Properties:
+    if algebra.dtype == bool:
+        x, y, z = (torch.rand(test_size * 3, 1) > 0.5).chunk(3, dim=0)
+    else:
+        x, y, z = torch.rand(test_size * 3, 1).chunk(3, dim=0)
+
+    meet, join, neg = algebra.meet, algebra.join, algebra.neg
+    top, bottom = algebra.top(), algebra.bottom()
+
+    meet_commutative = commutative(meet)(x, y)
+    join_commutative = commutative(join)(x, y)
+    meet_associative = associative(meet)(x, y, z)
+    join_associative = associative(join)(x, y, z)
+    meet_idempotent = idempotent(meet)(x)
+    join_idempotent = idempotent(join)(x)
+    absorption = all(
+        (torch.allclose(x, meet(x, join(x, y))),
+         torch.allclose(x, join(x, meet(x, y))))
+    )
+    distributivity = all(
+        (torch.allclose(meet(x, join(y, z)), join(meet(x, y), meet(x, z))),
+         torch.allclose(join(x, meet(y, z)), meet(join(x, y), join(x, z))))
+    )
+    involution = torch.allclose(x, neg(neg(x)))
+    de_morgan = all(
+        (torch.allclose(neg(join(x, y)), meet(neg(x), neg(y))),
+         torch.allclose(neg(meet(x, y)), join(neg(x), neg(y))))
+    )
+    complementarity = all(
+        (torch.allclose(meet(x, neg(x)), bottom),
+         torch.allclose(join(x, neg(x)), top))
+    )
+    return Properties(
+        meet_commutative=meet_commutative,
+        join_commutative=join_commutative,
+        meet_associative=meet_associative,
+        join_associative=join_associative,
+        meet_idempotent=meet_idempotent,
+        join_idempotent=join_idempotent,
+        absorption=absorption,
+        distributivity=distributivity,
+        involution=involution,
+        de_morgan=de_morgan,
+        complementarity=complementarity
+    )
+
+
 def algebra_factory(
+        _dtype: Type[bool] | Type[float],
         top: Tensor,
         bottom: Tensor,
         meet: Fn[[Tensor, Tensor], Tensor],
@@ -57,7 +145,7 @@ def algebra_factory(
         running_meet: Fn[[Tensor], Tensor] | None = None,
         running_join: Fn[[Tensor], Tensor] | None = None,
         exists: Fn[[Tensor], Tensor] | None = None,
-        forall: Fn[[Tensor], Tensor] | None = None
+        forall: Fn[[Tensor], Tensor] | None = None,
 ) -> Type[Algebra]:
 
     if running_meet is None:
@@ -69,7 +157,9 @@ def algebra_factory(
     if forall is None:
         forall = fold(meet, top)
 
-    class Instance(Algebra, ABC):
+    class Instance(Algebra[_dtype], ABC):
+        dtype = _dtype
+
         @classmethod
         def top(cls) -> Tensor: return top
         @classmethod
@@ -88,10 +178,17 @@ def algebra_factory(
         def exists(cls, x: Tensor) -> Tensor: return exists(x)
         @classmethod
         def forall(cls, x: Tensor) -> Tensor: return forall(x)
-    return Instance
+
+    def adorn(algebra: Type[Instance]) -> Type[Instance]:
+        class Adorned(algebra, ABC):
+            properties: Properties = check(algebra)
+        return Adorned
+
+    return adorn(Instance)
 
 
 Boolean = algebra_factory(
+    _dtype=bool,
     top=torch.tensor([True]),
     bottom=torch.tensor([False]),
     meet=torch.bitwise_and,
@@ -103,6 +200,7 @@ Boolean = algebra_factory(
     forall=lambda x: torch.all(x, dim=-1)
 )
 Goedel = algebra_factory(
+    _dtype=float,
     top=torch.tensor(1.),
     bottom=torch.tensor(0.),
     meet=torch.minimum,
@@ -114,6 +212,7 @@ Goedel = algebra_factory(
     forall=lambda x: torch.min(x, dim=-1).values
 )
 Product = algebra_factory(
+    _dtype=float,
     top=torch.tensor(1.),
     bottom=torch.tensor(0.),
     meet=torch.mul,
@@ -123,6 +222,7 @@ Product = algebra_factory(
     forall=lambda x: torch.prod(x, -1),
 )
 Lukasiewicz = algebra_factory(
+    _dtype=float,
     top=torch.tensor(1.),
     bottom=torch.tensor(0.),
     meet=lambda x, y: torch.clamp(x + y - 1, min=0.),
@@ -130,6 +230,7 @@ Lukasiewicz = algebra_factory(
     neg=lambda x: 1 - x
 )
 Hamacher = algebra_factory(
+    _dtype=float,
     top=torch.tensor(1.),
     bottom=torch.tensor(0.),
     meet=lambda x, y: torch.where(torch.bitwise_or(torch.eq(x, 0), torch.eq(y, 0)) , 0., (x + y) / (x + y - x * y)),
