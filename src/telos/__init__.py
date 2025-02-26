9a4355eb45792fd37977f5528c0e49581345fc25
konstantinosKokos <konstantinos@riseup.net>
2025-02-26 17:39:30 +0100

    remove Frank and Hamacher for now

--- src/telos/__init__.py
+++ src/telos/__init__.py
@@ -3,4 +3,4 @@ from .syntax import (
     Negation, Next, Disjunction, Until, Conjunction, Implies,
     eventually, always, iff)
 from .deduction import Trace, Judgement, model
-from .algebras import Algebra, Boolean, Goedel, Product, Lukasiewicz, Hamacher, Frank
+from .algebras import Algebra, Boolean, Goedel, Product, Lukasiewicz
--- src/telos/algebras/__init__.py
+++ src/telos/algebras/__init__.py
@@ -3,5 +3,3 @@ from .goedel import Goedel
 from .boolean import Boolean
 from .product import Product
 from .lukasiewicz import Lukasiewicz
-from .hamacher import Hamacher
-from .frank import Frank
