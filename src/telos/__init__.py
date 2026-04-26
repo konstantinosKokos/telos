dda59d8ed938dad075332cc7bea2ea1a404d0697
konstantinosKokos <konstantinos@riseup.net>
2026-04-27 00:14:15 +0300

    turn model into a Module

--- src/telos/__init__.py
+++ src/telos/__init__.py
@@ -2,5 +2,5 @@ from .syntax import (
     Formula, AbstractTop, AbstractBottom, Variable,
     Negation, Next, Disjunction, Until, Conjunction, Implies,
     eventually, always, iff)
-from .deduction import Trace, Judgement, model
-from .algebras import Algebra, Boolean, Goedel, Product, Lukasiewicz
+from .deduction import Trace, Judgement, Model
+from .algebras import Algebra, Boolean, Goedel, Product, Lukasiewicz, Robustness, Frank
--- src/telos/deduction.py
+++ src/telos/deduction.py
@@ -8,31 +8,40 @@ from .algebras import Algebra
 
 import torch
 from torch import Tensor
-from typing import Any, Protocol
+from torch.nn import Module
+from typing import Any
 from torch.nn.functional import pad
 
 
-class Trace(dict[Variable, Tensor]):
-    def __init__(self, mapping: dict[Variable, Tensor]):
-        assert len({v.size() for v in mapping.values()}) == 1
-        super().__init__(mapping)
+class Trace:
+    __match_args__ = ('names', 'values')
 
-    @property
-    def variables(self) -> set[Variable]:
-        return set(self.keys())
+    def __init__(self, **vars: Tensor):
+        assert vars, "Trace needs at least one variable."
+        assert len({t.size() for t in vars.values()}) == 1, "Variables must share shape."
+        self.names = tuple(Variable(k) for k in vars.keys())
+        self.values = torch.stack(list(vars.values()), dim=-2)
+
+    def __getitem__(self, key: Variable | str) -> Tensor:
+        if isinstance(key, str):
+            key = Variable(key)
+        return self.values[..., self.names.index(key), :]
 
     def __rshift__(self, other: Formula) -> Judgement:
         return Judgement(self, other)
 
-    def __len__(self) -> int:
-        return next(iter(self.values())).size(-1)
+    @property
+    def variables(self) -> set[Variable]:
+        return set(self.names)
+
+    @property
+    def duration(self) -> int:
+        return self.values.size(-1)
 
     def __eq__(self, other: Any) -> bool:
-        return all(
-            (isinstance(other, Trace),
-             self.variables == other.variables,
-             all(torch.all(self[k] == other[k]) for k in self.variables))
-        )
+        return (isinstance(other, Trace)
+                and self.names == other.names
+                and torch.equal(self.values, other.values))
 
 
 class Judgement:
@@ -48,42 +57,41 @@ class Judgement:
         return isinstance(other, Judgement) and all((self.trace == other.trace, self.conclusion == other.conclusion))
 
 
-class Model(Protocol):
-    def __call__(self, judgement: Judgement, return_trajectory: bool = False) -> Tensor: ...
+class Model(Module):
+    def __init__(self, algebra: Algebra):
+        super().__init__()
+        self.algebra = algebra
 
+    def forward(self, judgement: Judgement, return_trajectory: bool = False) -> Tensor:
+        algebra = self.algebra
 
-def model(algebra: Algebra) -> Model:
-    def go(trace: Trace, conclusion: Formula) -> Tensor:
+        def go(trace: Trace, conclusion: Formula) -> Tensor:
+            def reshape(result: Tensor) -> Tensor:
+                return result.expand_as(trace.values[..., 0, :])
 
-        def reshape(result: Tensor) -> Tensor:
-            return result.expand_as(next(iter(trace.values())))
+            match conclusion:
+                case AbstractTop():
+                    return reshape(algebra.top)
+                case AbstractBottom():
+                    return reshape(algebra.bottom)
+                case Variable(_):
+                    return trace[conclusion]
+                case Negation(x):
+                    return algebra.neg(go(trace, x))
+                case Next(x):
+                    return pad(go(trace, x)[..., 1:], pad=(0, 1), value=algebra.bottom)
+                case Disjunction(l, r):
+                    return algebra.join(go(trace, l), go(trace, r))
+                case Conjunction(l, r):
+                    return algebra.meet(go(trace, l), go(trace, r))
+                case Implies(l, r):
+                    return algebra.implies(go(trace, l), go(trace, r))
+                case Until(l, r):
+                    lss = algebra.span_meet(go(trace, l))
+                    rs = go(trace, r)[..., None, :]
+                    return algebra.exists(algebra.meet(lss, rs))
+                case _:
+                    raise ValueError
 
-        match conclusion:
-            case AbstractTop():
-                return reshape(algebra.top)
-            case AbstractBottom():
-                return reshape(algebra.bottom)
-            case Variable(x):
-                return trace[Variable(x)]
-            case Negation(x):
-                return algebra.neg(go(trace, x))
-            case Next(x):
-                return pad(go(trace, x)[..., 1:], pad=(0, 1), value=algebra.bottom)
-            case Disjunction(l, r):
-                return algebra.join(go(trace, l), go(trace, r))
-            case Conjunction(l, r):
-                return algebra.meet(go(trace, l), go(trace, r))
-            case Implies(l, r):
-                return algebra.implies(go(trace, l), go(trace, r))
-            case Until(l, r):
-                lss = algebra.span_meet(go(trace, l))
-                rs = go(trace, r)[..., None, :]
-                return algebra.exists(algebra.meet(lss, rs))
-            case _:
-                raise ValueError
-
-    def evaluate(judgement: Judgement, return_trajectory: bool = False) -> Tensor:
         result = go(judgement.trace, judgement.conclusion)
         return result if return_trajectory else result[..., 0]
-
-    return evaluate
