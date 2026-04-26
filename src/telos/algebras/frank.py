84b9d7eb153faaa9861ce28752448084e3ccaa7f
konstantinosKokos <konstantinos@riseup.net>
2026-04-27 00:14:41 +0300

    clamp frank parameter in call sites

--- src/telos/algebras/frank.py
+++ src/telos/algebras/frank.py
@@ -6,19 +6,22 @@ from .base import FuzzyBase
 
 
 class Frank(FuzzyBase):
-    def __init__(self, lam: float, trainable: bool):
+    def __init__(self, lam: float, trainable: bool, eps: float = 1e-3):
         super().__init__()
-        self.lam = Parameter(torch.tensor(lam), requires_grad=trainable)
+        self._lam = Parameter(torch.tensor(lam), requires_grad=trainable)
+        self.eps = eps
+
+    @property
+    def lam(self) -> Tensor:
+        return torch.clamp(self._lam, min=self.eps, max=1 - self.eps)
 
     def meet(self, x: Tensor, y: Tensor) -> Tensor:
-        lam = self.lam
-        return torch.log1p((lam ** x - 1) * (lam ** y - 1) / (lam - 1)) / torch.log(lam)
+        return torch.log1p((self.lam ** x - 1) * (self.lam ** y - 1) / (self.lam - 1)) / torch.log(self.lam)
 
     def join(self, x: Tensor, y: Tensor) -> Tensor:
         return 1 - self.meet(1 - x, 1 - y)
 
     def implies(self, x: Tensor, y: Tensor) -> Tensor:
-        lam = self.lam
-        x_safe = torch.where(x > 0, x, torch.ones_like(x))
-        r = torch.log1p((lam ** y - 1) * (lam - 1) / (lam ** x_safe - 1)) / torch.log(lam)
+        x_s = torch.where(x > 0, x, torch.ones_like(x))
+        r = torch.log1p((self.lam ** y - 1) * (self.lam - 1) / (self.lam ** x_s - 1)) / torch.log(self.lam)
         return torch.where(x <= y, self.top.expand_as(r), r)
